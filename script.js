b3fabdd50f13233f7918cc3000a9bc1209631012
Jakub.Dragowski <kuba.dragowski@gmail.com>
2022-12-05 02:30:11 +0100

    #added last changes

--- script.js
+++ script.js
@@ -12,20 +12,86 @@ const historyBtn = document.querySelector('.history-btn');
 
 let result = '';
 
-function displayNumbers(params) {}
+function displayNumbers() {
+  if (this.textContent === '.' && currNumber.innerHTML.includes('.')) return;
+  if (this.textContent === '.' && currNumber.innerHTML === '')
+    return (currNumber.innerHTML = '.0');
+  currNumber.innerHTML += this.textContent;
+}
 
-function operate() {}
+function operate() {
+  if (currNumber.innerHTML === '' && this.textContent === '-') {
+    currNumber.innerHTML = '-';
+    return;
+  } else if (currNumber.innerHTML === '') {
+    return;
+  }
 
-function showResult() {}
+  if (mathSign.innerHTML !== '') {
+    showResult();
+  }
+  prevNumber.innerHTML = currNumber.innerHTML;
+  mathSign.innerHTML = this.textContent;
+  currNumber.innerHTML = '';
+}
 
-function clearScreen() {}
+function showResult() {
+  if (prevNumber.innerHTML === '' || currNumber.innerHTML === '') return;
 
-function clearHistory() {}
+  let a = Number(currNumber.innerHTML);
+  let b = Number(prevNumber.innerHTML);
+  let operator = mathSign.innerHTML;
+
+  switch (operator) {
+    case '+':
+      result = a + b;
+      break;
+    case '-':
+      result = b - a;
+      break;
+    case 'x':
+      result = a * b;
+      break;
+    case ':':
+      result = b / a;
+      break;
+    case '2^':
+      result = b ** a;
+      break;
+  }
+
+  addToHistory();
+  historyBtn.classList.add('active');
+  currNumber.innerHTML = result;
+  prevNumber.innerHTML = '';
+  mathSign.innerHTML = '';
+}
+
+function addToHistory() {
+  const newHistoryItem = document.createElement('li');
+  newHistoryItem.innerHTML = `${currNumber.innerHTML} ${mathSign.innerHTML} ${prevNumber.innerHTML} = ${result}`;
+
+  newHistoryItem.classList.add('history-item');
+  calculatorHistory.appendChild(newHistoryItem);
+}
+
+function clearHistory() {
+  calculatorHistory.textContent = '';
+  if (calculatorHistory.textContent === '')
+    historyBtn.classList.remove('active');
+}
+
+function clearScreen() {
+  result = '';
+  currNumber.innerHTML = '';
+  prevNumber.innerHTML = '';
+  mathSign.innerHTML = '';
+}
 
 // Add Listener
 operatorsButtons.forEach((button) => button.addEventListener('click', operate));
 
-equalsButton.addEventListener('click', addEventListener('click', showResult));
+equalsButton.addEventListener('click', showResult);
 
 clearButton.addEventListener('click', clearScreen);
 
@@ -34,10 +100,3 @@ numbersButtons.forEach((button) => {
 });
 
 historyBtn.addEventListener('click', clearHistory);
-
-const clickMe = function () {
-  document.querySelector('.history-btn').addEventListener('click', () => {
-    console.log('Clicked');
-  });
-};
-clickMe();
